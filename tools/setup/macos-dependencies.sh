c3964d6251acc6e9c30973a3af0ba6f1b7c86800
Don Gagne <don@thegagnes.com>
2025-01-20 10:40:28 -0800

    Install GStreamer using dependency script

--- tools/setup/macos-dependencies.sh
+++ tools/setup/macos-dependencies.sh
@@ -9,3 +9,18 @@ fi
 
 brew update
 brew install cmake ninja ccache geographiclib git SDL2 exiv2 expat zlib shapelib pkgconf create-dmg
+
+# Install GStreamer
+GST_URL=https://gstreamer.freedesktop.org/data/pkg/osx
+GST_VERSION=1.22.12
+GST_DEV_PKG=gstreamer-1.0-devel-$GST_VERSION-universal.pkg
+GST_PKG=gstreamer-1.0-$GST_VERSION-universal.pkg
+pushd $TMPDIR
+curl -O $GST_URL/$GST_VERSION/$GST_DEV_PKG
+curl -O $GST_URL/$GST_VERSION/$GST_PKG
+echo "Sudo may be required to install GStreamer"
+sudo installer -verbose -pkg "$GST_DEV_PKG" -target /
+sudo installer -verbose -pkg "$GST_DEV_PKG" -target /
+rm $TMPDIR/$GST_DEV_PKG
+rm $TMPDIR/$GST_PKG
+popd
